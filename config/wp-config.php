4b20d94a3aa9918c988cd154fff95a023dc1fe3b
SlothOG <76158196+SlothOG@users.noreply.github.com>
2021-01-04 16:12:54 +0100

    Update wp-config.php

--- config/wp-config.php
+++ config/wp-config.php
@@ -20,10 +20,10 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'pigbenis' );
+define( 'DB_NAME', 'wordpress' );
 
 /** MySQL database username */
-define( 'DB_USER', 'tureuser' );
+define( 'DB_USER', 'test123' );
 
 /** MySQL database password */
 define( 'DB_PASSWORD', 'test123' );
