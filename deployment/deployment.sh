7185c8ef2b247ce8e369abb0024af6b4ca28f219
Pig Benis <terminal@localhost.localdomain>
2021-01-06 17:10:54 +0100

    aldaaaaaaaa mach das jetzt

--- deployment/deployment.sh
+++ deployment/deployment.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 #OS Pakete updaten
-if [$DISTRO=Fedora] then DISTRO=$(cat /etc/*-release | grep -w NAME | cut -d= -f2 | tr -d '"')
-echo "Current OS: $DISTRO"
+[$DISTRO=Fedora] DISTRO=$(cat /etc/*-release | grep -w NAME | cut -d= -f2 | tr -d '"')
+if echo "Current OS: $DISTRO"
+then 
 sudo dnf update
 #Install httpd
 sudo dnf -y install httpd mariadb-server mariadb php php-mysqlnd
@@ -13,12 +14,11 @@ systemctl start mariadb
 systemctl enable mariadb
 sh mysqlsetup.sh 
 mysql --user=root < create-database.sql
-wget https://de.wordpress.org/latest-de_DE.tar.gz
-mv latest-de_DE.tar.gz /var/www/
-cd /var/www/
-tar xvzf latest-de_DE.tar.gz
-mv ~/Doku/config/wp-config.php /var/www/wordpress/
-mv ~/Doku/wordpress /var/www/
+wget https://de.wordpress.org/latest-de_DE.tar.gz -P /tmp/
+cd /tmp/
+tar xvzf latest-de_DE.tar.gz -C /var/www/
+mv /home/terminal/Doku/config/wp-config.php /var/www/wordpress/
+mv /Doku/wordpress /var/www/
 sudo chown -R apache:apache wordpress
 mysql --password=test123 --user=test123 wordpress < ~/Doku/Database/pigbenis.sql
 fi
