bd6b2080c31329d69ad4cd911755f0eb8535fe1e
Pig Benis <terminal@localhost.localdomain>
2021-01-07 17:13:08 +0100

    asdasdasdsd

--- deployment/deployment.sh
+++ deployment/deployment.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #OS Pakete updaten
-[$DISTRO=Fedora] DISTRO=$(cat /etc/*-release | grep -w NAME | cut -d= -f2 | tr -d '"')
-if echo "Current OS: $DISTRO"
-then 
+DISTRO=$(cat /etc/*-release | grep -w NAME | cut -d= -f2 | tr -d '"')
+if echo [$DISTRO=Fedora] 
+"Current OS: $DISTRO" ; then 
 sudo dnf update
 #Install httpd
 sudo dnf -y install httpd mariadb-server mariadb php php-mysqlnd
@@ -15,13 +15,12 @@ systemctl enable mariadb
 sh mysqlsetup.sh 
 mysql --user=root < create-database.sql
 wget https://de.wordpress.org/latest-de_DE.tar.gz -P /tmp/
-cd /tmp/
-tar xvzf latest-de_DE.tar.gz -C /var/www/
-mv /home/terminal/Doku/config/wp-config.php /var/www/wordpress/
-mv /Doku/wordpress /var/www/
+tar xvzf /tmp/latest-de_DE.tar.gz -C /var/www/
+cp .. /home/terminal/Doku/config/wp-config.php /var/www/wordpress/
+mv .. /home/terminal/Doku/wordpress /var/www/
 sudo chown -R apache:apache wordpress
 mysql --password=test123 --user=test123 wordpress < ~/Doku/Database/pigbenis.sql
 fi
 
 else 
-echo "Wrong OS u tard"
+echo "Wrong OS u tard
