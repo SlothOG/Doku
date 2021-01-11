c49623edac8fb4f66651060889a3a54726fad57e
Pig Benis <terminal@localhost.localdomain>
2021-01-11 17:34:22 +0100

    PEEENIS

--- deployment/deployment.sh
+++ deployment/deployment.sh
@@ -7,19 +7,19 @@ then
 echo "Current OS: $DISTRO"  
 sudo dnf -y update
 #Install httpd
-sudo dnf -y install httpd mariadb-server mariadb php php-mysqlnd
+sudo dnf -y install httpd mariadb-server mariadb php php-mysqlnd wget
 #Dienste starten und für Autostart bereitstellen
 systemctl start httpd
 systemctl enable httpd
 systemctl start mariadb
 systemctl enable mariadb
-sh mysqlsetup.sh 
+sh mysql_se.sh 
 mysql --user=root < create-database.sql
-wget https://de.wordpress.org/latest-de_DE.tar.gz -P /tmp/
-tar xvzf /tmp/latest-de_DE.tar.gz -C /var/www/
-cp ../config/wp-config.php /var/www/wordpress/
-cp -r ../wordpress /var/www/
-sudo chown -R apache:apache /var/www/wordpress
+wget -nc https://de.wordpress.org/latest-de_DE.tar.gz -P /tmp/
+tar xvzf /tmp/latest-de_DE.tar.gz -C /var/www/html/
+cp ../config/wp-config.php /var/www/html/wordpress/
+cp -r ../wordpress /var/www/html/
+sudo chown -R apache:apache /var/www/html/
 mysql --password=test123 --user=test123 wordpress < ../Database/pigbenis.sql
 else
 echo "Wrong OS u tard"
