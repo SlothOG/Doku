34888a5d8906a87baa46599a18f1a35cc3db1fed
Pig Benis <terminal@pop-os.localdomain>
2021-01-21 16:46:21 +0100

    peeeeeenis

--- Dockerfile/container/scripts/create-database.sql
+++ Dockerfile/container/scripts/create-database.sql
@@ -1,4 +1,4 @@
 create Database wordpress;
-create user 'test123'@'%' identified by 'password';
+create user 'test123'@'%' identified by 'test123';
 use wordpress;
 grant all privileges on *.* to 'test123'@'%' identified by 'test123';
